002650f3d0ac25e33de7af4852339f18a6edcb67
kyleeardley <kyleeardley@gmail.com>
2017-09-21 00:15:14 -0600

    take cosmo off

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,7 +22,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    app.baseUrl = '/cosmo/';
+    // app.baseUrl = '/cosmo/';
   }
 
   app.displayInstalledToast = function() {
